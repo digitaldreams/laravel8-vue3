d2ce92a80062a2cdc09f97de29c81e32ba6e5f8a
Tuhin Bepari <digitaldreams40@gmail.com>
2021-04-20 16:59:50 +0600

    Init Repository

---
+++ resources/js/app.js
@@ -0,0 +1,5 @@
+require('./bootstrap');
+
+import { createApp } from 'vue';
+import App from './components/App.vue'
+createApp(App).mount("#app")
---
+++ resources/js/components/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="flex flex--column flex--align-center flex--justify-center">
+        <div class="logos">
+            <img src="../../static/img/laravel.png" width="240" alt="" />
+            <img src="../../static/img/vue.png" width="240" alt="" />
+        </div>
+
+        <div class="title">
+            <h1 class="vue">Vue 3</h1>
+
+            <h1 class="plus">+</h1>
+            <h1 class="laravel">Laravel 8</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log("Component mounted.");
+    },
+};
+</script>
+
